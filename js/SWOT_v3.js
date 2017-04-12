8756fc491bb3193c7ff45b73df9fe7449777f1cf
jpotts-IIL <joel.potts@innovaintegra.com>
2017-04-12 15:05:19 +0100

    Added the ability to remove values from lists
    Added the ability to remove items from lists
    Added an about page
    Some css changes
    Minor bug fixes

--- js/SWOT_v3.js
+++ js/SWOT_v3.js
@@ -3,7 +3,10 @@ var weakness = [];
 var opportunities = [];
 var threats = [];
 
-function addValue(value_type)
+/*
+This function adds a new value to the specified list (strength,weakness,opportunities, threat) and creates the html for it
+*/
+function addToList(value_type)
 {
 	switch(value_type)
 	{
@@ -11,38 +14,111 @@ function addValue(value_type)
 			if(document.getElementById("strength_input").value != null && document.getElementById("strength_input").value != "")
 			{
 				strengths.push(document.getElementById("strength_input").value);
+				document.getElementById("strength_ul").innerHTML = document.getElementById("strength_ul").innerHTML.concat("<li><span>" + document.getElementById("strength_input").value + "</span> <i onclick=\"removeFromList('strength', this)\" class=\"fa fa-times-circle remove-button\" aria-hidden=\"true\"></i></li>");
 				document.getElementById("strength_input").value = "";
-				document.getElementById("strength_list").innerHTML = strengths.toString().replace(/,/g,"<br>");
 			}
 			break;
 		case "weakness":
 			if(document.getElementById("weakness_input").value != null && document.getElementById("weakness_input").value != "")
 			{
 				weakness.push(document.getElementById("weakness_input").value);
+				document.getElementById("weakness_ul").innerHTML = document.getElementById("weakness_ul").innerHTML.concat("<li><span>" + document.getElementById("weakness_input").value + "</span> <i onclick=\"removeFromList('weakness', this)\" class=\"fa fa-times-circle remove-button\" aria-hidden=\"true\"></i></li>");
 				document.getElementById("weakness_input").value = "";
-				document.getElementById("weakness_list").innerHTML = weakness.toString().replace(/,/g,"<br>");
 			}
 			break;
 		case "opportunities":
 			if(document.getElementById("opportunities_input").value != null && document.getElementById("opportunities_input").value != "")
 			{
 				opportunities.push(document.getElementById("opportunities_input").value);
+				document.getElementById("opportunities_ul").innerHTML = document.getElementById("opportunities_ul").innerHTML.concat("<li><span>" + document.getElementById("opportunities_input").value + "</span> <i onclick=\"removeFromList('opportunities', this)\" class=\"fa fa-times-circle remove-button\" aria-hidden=\"true\"></i></li>");
 				document.getElementById("opportunities_input").value = "";
-				document.getElementById("opportunities_list").innerHTML = opportunities.toString().replace(/,/g,"<br>");
 			}
 			break;
 		case "threat":
 			if(document.getElementById("threat_input").value != null && document.getElementById("threat_input").value != "")
 			{
 				threats.push(document.getElementById("threat_input").value);
+				document.getElementById("threat_ul").innerHTML = document.getElementById("threat_ul").innerHTML.concat("<li><span>" + document.getElementById("threat_input").value + "</span> <i onclick=\"removeFromList('threat', this)\" class=\"fa fa-times-circle remove-button\" aria-hidden=\"true\"></i></li>");
 				document.getElementById("threat_input").value = "";
-				document.getElementById("threat_list").innerHTML = threats.toString().replace(/,/g,"<br>");
 			}
 			break;
 	}
-	
 }
 
+/*
+This function removes a specified value from a specified list and then modifies the html to reflect the change.
+*/
+function removeFromList(value_type, element)
+{
+	console.log("removing");
+	switch(value_type)
+	{
+		case "strength":
+			if(strengths.length > 0)
+			{
+				var li_element = element.parentNode;
+				var span_element = element.previousElementSibling;
+				var index = strengths.indexOf(span_element.innerHTML);
+				strengths.splice(index, 1);
+				document.getElementById("strength_ul").innerHTML = "";
+
+				for(i = 0; i < strengths.length; i++)
+				{
+					document.getElementById("strength_ul").innerHTML = document.getElementById("strength_ul").innerHTML.concat("<li><span>" + strengths[i] + "</span> <i onclick=\"removeFromList('strength', this)\" class=\"fa fa-times-circle remove-button\" aria-hidden=\"true\"></i></li>");
+				}
+			}
+			break;
+		case "weakness":
+			if(weakness.length > 0)
+			{
+				var li_element = element.parentNode;
+				var span_element = element.previousElementSibling;
+				var index = weakness.indexOf(span_element.innerHTML);
+				weakness.splice(index, 1);
+				document.getElementById("weakness_ul").innerHTML = "";
+
+				for(i = 0; i < weakness.length; i++)
+				{
+					document.getElementById("weakness_ul").innerHTML = document.getElementById("weakness_ul").innerHTML.concat("<li><span>" + weakness[i] + "</span> <i onclick=\"removeFromList('weakness', this)\" class=\"fa fa-times-circle remove-button\" aria-hidden=\"true\"></i></li>");
+				}
+			}
+			break;
+		case "opportunities":
+			if(opportunities.length > 0)
+			{
+				var li_element = element.parentNode;
+				var span_element = element.previousElementSibling;
+				var index = opportunities.indexOf(span_element.innerHTML);
+				opportunities.splice(index, 1);
+				document.getElementById("opportunities_ul").innerHTML = "";
+
+				for(i = 0; i < opportunities.length; i++)
+				{
+					document.getElementById("opportunities_ul").innerHTML = document.getElementById("opportunities_ul").innerHTML.concat("<li><span>" + opportunities[i] + "</span> <i onclick=\"removeFromList('opportunities', this)\" class=\"fa fa-times-circle remove-button\" aria-hidden=\"true\"></i></li>");
+				}
+			}
+			break;
+		case "threat":
+			if(threats.length > 0)
+			{
+				var li_element = element.parentNode;
+				var span_element = element.previousElementSibling;
+				var index = threats.indexOf(span_element.innerHTML);
+				threats.splice(index, 1);
+				document.getElementById("threat_ul").innerHTML = "";
+
+				for(i = 0; i < threats.length; i++)
+				{
+					document.getElementById("threat_ul").innerHTML = document.getElementById("threat_ul").innerHTML.concat("<li><span>" + threats[i] + "</span> <i onclick=\"removeFromList('threat', this)\" class=\"fa fa-times-circle remove-button\" aria-hidden=\"true\"></i></li>");
+				}
+			}
+			break;
+	}
+}
+
+/*
+creates a text file of the current data and saves it to disk
+*/
 function saveTextAsFile()
 {
 	var output = "";
@@ -67,11 +143,18 @@ function saveTextAsFile()
 	downloadLink.click();
 }
 
+/*
+destroys element that triggered the event
+*/
 function destroyClickedElement(event)
 {
 	document.body.removeChild(event.target);
 }
 
+/*
+This creates an input element of type 'file' and clicks it.
+The effect is a file selction pop-up.
+*/
 function createAndClickFile()
 {
 	var test = document.createElement("input");
@@ -83,8 +166,12 @@ function createAndClickFile()
 	test.click();
 }
 
+/*
+This method loads a text file from disk and adds its contents to the lists on the page.
+*/
 function loadFileAsText()
 {
+	console.log("loadFIleastext");
 	var fileToLoad = document.getElementById("createdfilebutton").files[0];
 	document.body.removeChild(document.getElementById("createdfilebutton"));
 
@@ -119,56 +206,36 @@ function loadFileAsText()
 				threats.push(thr[i]);
 			}
 		}
-		document.getElementById("strength_list").innerHTML = strengths.toString().replace(/,/g,"<br>");
-		document.getElementById("weakness_list").innerHTML = weakness.toString().replace(/,/g,"<br>");
-		document.getElementById("opportunities_list").innerHTML = opportunities.toString().replace(/,/g,"<br>");
-		document.getElementById("threat_list").innerHTML = threats.toString().replace(/,/g,"<br>");
+		console.log(strengths.length);
+		for(i = 0; i < strengths.length; i++)
+		{
+			document.getElementById("strength_ul").innerHTML = document.getElementById("strength_ul").innerHTML.concat("<li><span>" + strengths[i] + "</span> <i onclick=\"removeFromList('strength', this)\" class=\"fa fa-times-circle remove-button\" aria-hidden=\"true\"></i></li>");
+		}
+		for(i = 0; i < weakness.length; i++)
+		{
+			document.getElementById("weakness_ul").innerHTML = document.getElementById("weakness_ul").innerHTML.concat("<li><span>" + weakness[i] + "</span> <i onclick=\"removeFromList('weakness', this)\" class=\"fa fa-times-circle remove-button\" aria-hidden=\"true\"></i></li>");
+		}
+		for(i = 0; i < opportunities.length; i++)
+		{
+			document.getElementById("opportunities_ul").innerHTML = document.getElementById("opportunities_ul").innerHTML.concat("<li><span>" + opportunities[i] + "</span> <i onclick=\"removeFromList('opportunities', this)\" class=\"fa fa-times-circle remove-button\" aria-hidden=\"true\"></i></li>")
+		}
+		for(i = 0; i < threats.length; i++)
+		{
+			document.getElementById("threat_ul").innerHTML = document.getElementById("threat_ul").innerHTML.concat("<li><span>" + threats[i] + "</span> <i onclick=\"removeFromList('threat', this)\" class=\"fa fa-times-circle remove-button\" aria-hidden=\"true\"></i></li>");
+		}
 	};
 	fileReader.readAsText(fileToLoad, "UTF-8");
 	//document.body.removeChild(test);
 }
 
-function printWithBullets()
-{
-	var str = "";
-	for(i = 0; i < strengths.length; i++)
-	{
-		str += "<li>"+strengths[i]+"</li>";
-	}
-	var wea = "";
-	for(i = 0; i < weakness.length; i++)
-	{
-		wea += "<li>"+weakness[i]+"</li>";
-	}
-	var opp = "";
-	for(i = 0; i < opportunities.length; i++)
-	{
-		opp += "<li>"+opportunities[i]+"</li>";
-	}
-	var thr = "";
-	for(i = 0; i < threats.length; i++)
-	{
-		thr += "<li>"+threats[i]+"</li>";
-	}
-	document.getElementById("strength_list").innerHTML = "<ul>" + str + "</ul>";
-	document.getElementById("weakness_list").innerHTML = wea;
-	document.getElementById("opportunities_list").innerHTML = opp;
-	document.getElementById("threat_list").innerHTML = thr;
-}
-
-function printWithoutBullets()
-{
-	document.getElementById("strength_list").innerHTML = strengths.toString().replace(/,/g,"<br>");
-	document.getElementById("weakness_list").innerHTML = weakness.toString().replace(/,/g,"<br>");
-	document.getElementById("opportunities_list").innerHTML = opportunities.toString().replace(/,/g,"<br>");
-	document.getElementById("threat_list").innerHTML = threats.toString().replace(/,/g,"<br>");
-}
-
+/*
+creates a seperate window with a more print friendly layout.
+*/
 function printPDF()
 {
-	printWithBullets();
+	//printWithBullets();
 	var divContents = document.getElementById("swot_div").outerHTML;
-	printWithoutBullets();
+	//printWithoutBullets();
 	var printWindow = window.open('', '', 'height=400,width=800');
 	printWindow.document.write('<html><head><title>DIV Contents</title>');
 	printWindow.document.write('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">');
